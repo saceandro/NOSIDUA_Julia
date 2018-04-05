2204fadc939c373bc08dea567d6b37cf980531a9
Sace Andro <nskia_bens_cbw@yahoo.co.jp>
2018-04-05 17:49:37 +0900

    removed comments

--- test/Lorenz96/model.jl
+++ test/Lorenz96/model.jl
@@ -1,10 +1,3 @@
-# struct Lorenz{N, L, T<:AbstractFloat, A<:AbstractVecor, B<:AbstractMatrix, C<:AbstractArray}
-#     dxdt::A
-#     jacobian:::B
-#     hessian::C
-#     Lorenz{N,L,T,A,B,C}(dxdt::AbstractVector{T}, jacobian::AbstractMatrix{T}, hessian::AbstractArray{T,3}) where {N,L,T,A,B,C} = new{N,L,T,A,B,C}(dxdt, jacobian, hessian)
-# end
-
 function dxdt!(a::Adjoint{N}, t, x) where {N}
     a.dxdt[1]     = a.p[2] * (x[2]   - x[N-1]) * x[N]   + a.p[1] - x[1]
     a.dxdt[2]     = a.p[2] * (x[3]   - x[N])   * x[1]   + a.p[1] - x[2]
@@ -14,19 +7,6 @@ function dxdt!(a::Adjoint{N}, t, x) where {N}
     a.dxdt[N]     = a.p[2] * (x[1]   - x[N-2]) * x[N-1] + a.p[1] - x[N]
     nothing
 end
-
-# function jacobian!(a::Adjoint{N, L}, t, x) where {N, L} # might be faster if SparseMatrixCSC is used. L=N+M.
-#     println((N+1 == 6) + 0.)
-#     for j in 1:L, i in 1:N
-#         a.jacobian[i,j] = a.p[2]     * ( (mod1(i+1, N) == j) -  (mod1(i-2, N) == j)) * x[mod1(i-1, N)]
-#                         + a.p[2]     * (x[mod1(i+1, N)]      - x[mod1(i-2, N)])      *  (mod1(i-1, N) == j)
-#                         + (N+2 == j) * (x[mod1(i+1, N)]      - x[mod1(i-2, N)])      * x[mod1(i-1, N)]
-#                         + (N+1 == j)
-#                         - (i   == j)
-#     println(a.jacobian)
-#     end
-#     nothing
-# end
 
 function jacobian!(a::Adjoint{N}, t, x) where {N} # might be faster if SparseMatrixCSC is used. L=N+M. need to write in one line
     for j in 1:N, i in 1:N
@@ -38,18 +18,6 @@ function jacobian!(a::Adjoint{N}, t, x) where {N} # might be faster if SparseMat
     end
     nothing
 end
-
-# function hessian!(a::Adjoint{N, L}, t, x) where {N, L}
-#     for k in 1:L, j in 1:L, i in 1:N
-#         a.hessian[i,j,k] = (N+2 == j) * ( (mod1(i+1, N)==k) -  (mod1(i-2, N)==k)) * x[mod1(i-1, N)]
-#                          + (N+2 == j) * (x[mod1(i+1, N)]    - x[mod1(i-2, N)])    *  (mod1(i-1, N)==k)
-#                          + (N+2 == k) * ( (mod1(i+1, N)==j) -  (mod1(i-2, N)==j)) * x[mod1(i-1, N)]
-#                          + a.p[2]     * ( (mod1(i+1, N)==j) -  (mod1(i-2, N)==j)) *  (mod1(i-1, N)==k)
-#                          + (N+2 == k) * (x[mod1(i+1, N)]    - x[mod1(i-2, N)])    *  (mod1(i-1, N)==j)
-#                          + a.p[2]     * ( (mod1(i+1, N)==k) -  (mod1(i-2, N)==k)) *  (mod1(i-1, N)==j)
-#     end
-#     nothing
-# end
 
 function hessian!(a::Adjoint{N}, t, x) where {N}
     for k in 1:N, j in 1:N, i in 1:N
