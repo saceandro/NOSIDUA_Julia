b70093e6965af31def2313460952caf41b5ee217
Sace Andro <nskia_bens_cbw@yahoo.co.jp>
2018-04-10 15:03:19 +0900

    added @inbounds and removed @simd from model

--- test/Lorenz96/model.jl
+++ test/Lorenz96/model.jl
@@ -2,7 +2,7 @@ function dxdt!(dxdt, t, x, p)
     N = length(dxdt)
     dxdt[1]     = p[2] * (x[2]   - x[N-1]) * x[N]   + p[1] - x[1]
     dxdt[2]     = p[2] * (x[3]   - x[N])   * x[1]   + p[1] - x[2]
-    @simd for i in 3:N-1
+    @inbounds for i in 3:N-1
         dxdt[i] = p[2] * (x[i+1] - x[i-2]) * x[i-1] + p[1] - x[i]
     end
     dxdt[N]     = p[2] * (x[1]   - x[N-2]) * x[N-1] + p[1] - x[N]
@@ -11,11 +11,11 @@ end
 
 function jacobian!(jacobian, t, x, p) # might be faster if SparseMatrixCSC is used. L=N+M. need to write in one line
     N = size(jacobian, 1)
-    for j in 1:N, i in 1:N
+    @inbounds for j in 1:N, i in 1:N
         jacobian[i,j] = p[2]     * ( (mod1(i+1, N) == j) -  (mod1(i-2, N) == j)) * x[mod1(i-1, N)] + p[2]     * (x[mod1(i+1, N)]      - x[mod1(i-2, N)])      *  (mod1(i-1, N) == j) - (i   == j)
     end
     jacobian[:,N+1] .= 1.
-    @simd for i in 1:N
+    @inbounds for i in 1:N
         jacobian[i,N+2] = (x[mod1(i+1, N)]      - x[mod1(i-2, N)])      * x[mod1(i-1, N)]
     end
     nothing
@@ -23,13 +23,13 @@ end
 
 function hessian!(hessian, t, x, p)
     N = size(hessian, 1)
-    for k in 1:N, j in 1:N, i in 1:N
+    @inbounds for k in 1:N, j in 1:N, i in 1:N
         hessian[i,j,k] = p[2]     * ( (mod1(i+1, N)==j) -  (mod1(i-2, N)==j)) *  (mod1(i-1, N)==k) + p[2]     * ( (mod1(i+1, N)==k) -  (mod1(i-2, N)==k)) *  (mod1(i-1, N)==j)
     end
-    for j in 1:N, i in 1:N
+    @inbounds for j in 1:N, i in 1:N
         hessian[i,j,N+2] = ( (mod1(i+1, N)==j) -  (mod1(i-2, N)==j)) * x[mod1(i-1, N)] + (x[mod1(i+1, N)]    - x[mod1(i-2, N)])    *  (mod1(i-1, N)==j)
     end
-    for k in 1:N, i in 1:N
+    @inbounds for k in 1:N, i in 1:N
         hessian[i,N+2,k] = hessian[i,k,N+2]
     end
     nothing
