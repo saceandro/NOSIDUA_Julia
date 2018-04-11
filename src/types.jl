02cc555ce162ee282aeda115d947315f1be10f9a
Sace Andro <nskia_bens_cbw@yahoo.co.jp>
2018-04-11 15:40:13 +0900

    separated model and adjoint type

--- src/types.jl
+++ src/types.jl
@@ -1,4 +1,6 @@
-mutable struct Model{N, L, T<:AbstractFloat, A<:AbstractVector, B<:AbstractMatrix, C<:AbstractArray, F<:Function, G<:Function, H<:Function}
+# abstract type AbstractModel{N, L, T, A, B, C, F, G, H} where {T<:AbstractFloat, A<:AbstractVector, B<:AbstractMatrix, C<:AbstractArray, F<:Function, G<:Function, H<:Function} end
+
+mutable struct Model{N, L, T<:AbstractFloat, A<:AbstractVector, B<:AbstractMatrix, C<:AbstractArray, F<:Function, G<:Function, H<:Function} #<: AbstractModel{N, L, T, A, B, C, F, G, H}
     dxdt::A
     jacobian::B
     hessian::C
@@ -8,14 +10,14 @@ mutable struct Model{N, L, T<:AbstractFloat, A<:AbstractVector, B<:AbstractMatri
     Model{N, L, T, A, B, C, F, G, H}(dxdt::AbstractVector{T}, jacobian::AbstractMatrix{T}, hessian::AbstractArray{T,3}, dxdt!::F, jacobian!::G, hessian!::H) where {N,L,T,A,B,C,F,G,H} = new{N,L,T,A,B,C,F,G,H}(dxdt, jacobian, hessian, dxdt!, jacobian!, hessian!)
 end
 
-function Model(t::Type{T}, N::Int, L::Int, dxdt!::F, jacobian!::G, hessian!::H) where {T<:AbstractFloat, F<:Function, G<:Function, H<:Function}
+function Model(t::Type{T}, N, L, dxdt!::F, jacobian!::G, hessian!::H) where {T<:AbstractFloat, F<:Function, G<:Function, H<:Function}
     dxdt = Array{t}(N)
     jacobian = zeros(t, N, L)
     hessian = zeros(t, N, L, L)
     Model{N, L, t, typeof(dxdt), typeof(jacobian), typeof(hessian), typeof(dxdt!), typeof(jacobian!), typeof(hessian!)}(dxdt, jacobian, hessian, dxdt!, jacobian!, hessian!)
 end
 
-mutable struct Adjoint{N, L, T<:AbstractFloat, A<:AbstractVector, B<:AbstractMatrix}
+mutable struct Adjoint{N, L, T<:AbstractFloat, A<:AbstractVector, B<:AbstractMatrix, M<:Model}
     dt::T
     steps::Int
     obs_variance::T
@@ -26,14 +28,14 @@ mutable struct Adjoint{N, L, T<:AbstractFloat, A<:AbstractVector, B<:AbstractMat
     dp::A
     λ::B
     dλ::B
-    model::Model
-    Adjoint{N, L, T, A, B}(dt::T, steps::Int, obs_variance::T, obs::AbstractMatrix{T}, x::AbstractMatrix{T}, p::AbstractVector{T}, dx::AbstractMatrix{T}, dp::AbstractVector{T}, λ::AbstractMatrix{T}, dλ::AbstractMatrix{T}, model::Model) where {N,L,T,A,B} = new{N,L,T,A,B}(dt, steps, obs_variance, obs, x, p, dx, dp, λ, dλ, model)
+    model::M
+    Adjoint{N, L, T, A, B, M}(dt::T, steps::Int, obs_variance::T, obs::AbstractMatrix{T}, x::AbstractMatrix{T}, p::AbstractVector{T}, dx::AbstractMatrix{T}, dp::AbstractVector{T}, λ::AbstractMatrix{T}, dλ::AbstractMatrix{T}, model::M) where {N,L,T,A,B,M} = new{N,L,T,A,B,M}(dt, steps, obs_variance, obs, x, p, dx, dp, λ, dλ, model)
 end
 
 function Adjoint(dt::T, steps::Int, obs_variance::T, obs::AbstractMatrix{T}, x::AbstractMatrix{T}, p::AbstractVector{T}, dx::AbstractMatrix{T}, dp::AbstractVector{T}, λ::AbstractMatrix{T}, dλ::AbstractMatrix{T}, model::Model) where {T<:AbstractFloat}
     xdim = size(x,1)
     θdim = xdim + length(p)
-    Adjoint{xdim, θdim, T, typeof(p), typeof(x)}(dt, steps, obs_variance, obs, x, p, dx, dp, λ, dλ, model)
+    Adjoint{xdim, θdim, T, typeof(p), typeof(x), typeof(model)}(dt, steps, obs_variance, obs, x, p, dx, dp, λ, dλ, model)
 end
 
 function Adjoint(dt::T, steps::Int, obs_variance::T, obs::AbstractMatrix{T}, x0::AbstractVector{T}, p::AbstractVector{T}, dx0::AbstractVector{T}, dp::AbstractVector{T}, model::Model) where {T<:AbstractFloat}
