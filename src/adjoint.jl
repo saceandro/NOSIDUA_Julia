7006997adbca9d8f903f884d2f417d6405edea6a
Sace Andro <nskia_bens_cbw@yahoo.co.jp>
2018-04-11 17:02:07 +0900

    finished

--- src/adjoint.jl
+++ src/adjoint.jl
@@ -4,25 +4,25 @@ innovation_λ(  x, obs, obs_variance) = isnan(obs) ? zero(x)  : (x - obs)/obs_va
 
 innovation_dλ(dx, obs, obs_variance) = isnan(obs) ? zero(dx) : dx/obs_variance        # element-wise innovation of dλ.
 
-next_x!(        a::Adjoint,       m::Model,       t, x, x_nxt)                                               = (m.dxdt!(    m.dxdt,     t, x, a.p);                                     x_nxt .=  x .+ m.dxdt                                                             .* a.dt; nothing)
+next_x!(        m::Model,       dt, t, x, p, x_nxt)                                                 = (m.dxdt!(    m.dxdt,     t, x, p);                                         x_nxt .=  x .+ m.dxdt                                                           .* dt; nothing)
 
-next_dx!(       a::Adjoint,       m::Model,       t, x,        dx, dx_nxt)                                   = (m.jacobian!(m.jacobian, t, x, a.p);                                    dx_nxt .= dx .+ m.jacobian  * CatView(dx, a.dp)                                    .* a.dt; nothing)
+next_dx!(       m::Model,       dt, t, x, p,       dx, dp, dx_nxt)                                  = (m.jacobian!(m.jacobian, t, x, p);                                        dx_nxt .= dx .+ m.jacobian  * CatView(dx, dp)                                    .* dt; nothing)
 
-@views prev_λ!( a::Adjoint{N},    m::Model{N},    t, x,                   λ, λ_prev)            where {N}    = (m.jacobian!(m.jacobian, t, x, a.p);                                    λ_prev .=  λ .+ m.jacobian' *  λ[1:N]                                              .* a.dt; nothing)
+@views prev_λ!( m::Model{N},    dt, t, x, p,                     λ, λ_prev)            where {N}    = (m.jacobian!(m.jacobian, t, x, p);                                        λ_prev .=  λ .+ m.jacobian' *  λ[1:N]                                            .* dt; nothing)
 
-@views prev_dλ!(a::Adjoint{N, L}, m::Model{N, L}, t, x,        dx,        λ,       dλ, dλ_prev) where {N, L} = (m.hessian!( m.hessian,  t, x, a.p); prev_λ!(a, m, t, x, dλ, dλ_prev); dλ_prev .+= reshape(reshape(m.hessian, N*L, L) * CatView(dx, a.dp), N, L)' * λ[1:N] .* a.dt; nothing)
+@views prev_dλ!(m::Model{N, L}, dt, t, x, p,       dx, dp,       λ,       dλ, dλ_prev) where {N, L} = (m.hessian!( m.hessian,  t, x, p); prev_λ!(m, dt, t, x, p, dλ, dλ_prev); dλ_prev .+= reshape(reshape(m.hessian, N*L, L) * CatView(dx, dp), N, L)' * λ[1:N] .* dt; nothing)
 
 
 @views function orbit!(a, m)
     for _i in 1:a.steps
-        next_x!(a, m, a.dt*(_i-1), a.x[:,_i], a.x[:,_i+1])
+        next_x!(m, a.dt, a.dt*(_i-1), a.x[:,_i], a.p, a.x[:,_i+1])
     end
     nothing
 end
 
 @views function neighboring!(a, m)
     for _i in 1:a.steps
-        next_dx!(a, m, a.dt*(_i-1), a.x[:,_i],            a.dx[:,_i], a.dx[:,_i+1])
+        next_dx!(m, a.dt, a.dt*(_i-1), a.x[:,_i], a.p,           a.dx[:,_i], a.dp, a.dx[:,_i+1])
     end
     nothing
 end
@@ -30,7 +30,7 @@ end
 @views function gradient!(a::Adjoint{N}, m::Model{N}) where {N} # assuming x[:,1] .= x0; p .= p; orbit!(dxdt, t, x, p, dt); is already run. λ[:,1] is the gradient.
     a.λ[1:N,end] .= innovation_λ.(a.x[:,end], a.obs[:,end], a.obs_variance)
     for _i in a.steps:-1:1
-        prev_λ!(a, m, a.dt*(_i-1), a.x[:,_i],                                      a.λ[:,_i+1], a.λ[:,_i]) # fix me! a.dt*_i?
+        prev_λ!(m, a.dt, a.dt*(_i-1), a.x[:,_i], a.p,                                      a.λ[:,_i+1], a.λ[:,_i]) # fix me! a.dt*_i?
         a.λ[1:N,_i] .+= innovation_λ.(a.x[:,_i], a.obs[:,_i], a.obs_variance)
     end
     nothing
@@ -56,7 +56,7 @@ end
 @views function hessian_vector_product!(a::Adjoint{N}, m::Model{N}) where {N} # assuming dx[:,1] .= dx0; dp .= dp; neighboring!(jacobian, dt, t, x, p, dx, dp); is already run. dλ[:,1] is the hessian_vector_product.
     a.dλ[1:N,end] .= innovation_dλ.(a.dx[:,end], a.obs[:,end], a.obs_variance)
     for _i in a.steps:-1:1
-        prev_dλ!(a, m, a.dt*(_i-1), a.x[:,_i],            a.dx[:,_i],              a.λ[:,_i+1],            a.dλ[:,_i+1], a.dλ[:,_i]) # fix me! a.dt*_i?
+        prev_dλ!(m, a.dt, a.dt*(_i-1), a.x[:,_i], a.p,           a.dx[:,_i], a.dp,             a.λ[:,_i+1],            a.dλ[:,_i+1], a.dλ[:,_i]) # fix me! a.dt*_i?
         a.dλ[1:N,_i] .+= innovation_dλ.(a.dx[:,_i], a.obs[:,_i], a.obs_variance)
     end
     nothing
