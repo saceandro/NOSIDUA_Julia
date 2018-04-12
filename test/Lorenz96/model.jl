3bbd5e46c5948ffc3cf24be5e14fc48cbf54f483
Sace Andro <nskia_bens_cbw@yahoo.co.jp>
2018-04-12 15:03:24 +0900

    finished

--- test/Lorenz96/model.jl
+++ test/Lorenz96/model.jl
@@ -1,4 +1,4 @@
-function dxdt!(m::Model{N}, t, x, p)
+function dxdt!(m::Model{N}, t, x, p) where {N}
     @inbounds m.dxdt[1]     = p[2] * (x[2]   - x[N-1]) * x[N]   + p[1] - x[1]
     @inbounds m.dxdt[2]     = p[2] * (x[3]   - x[N])   * x[1]   + p[1] - x[2]
     @inbounds for i in 3:N-1
@@ -8,7 +8,7 @@ function dxdt!(m::Model{N}, t, x, p)
     nothing
 end
 
-function jacobian!(m::Model{N}, t, x, p) # might be faster if SparseMatrixCSC is used. L=N+M. need to write in one line
+function jacobian!(m::Model{N}, t, x, p) where {N} # might be faster if SparseMatrixCSC is used. L=N+M. need to write in one line
     @inbounds for j in 1:N, i in 1:N
         m.jacobian[i,j] = p[2]     * ( (mod1(i+1, N) == j) -  (mod1(i-2, N) == j)) * x[mod1(i-1, N)] + p[2]     * (x[mod1(i+1, N)]      - x[mod1(i-2, N)])      *  (mod1(i-1, N) == j) - (i   == j)
     end
@@ -19,7 +19,7 @@ function jacobian!(m::Model{N}, t, x, p) # might be faster if SparseMatrixCSC is
     nothing
 end
 
-function hessian!(m::Model{N}, t, x, p)
+function hessian!(m::Model{N}, t, x, p) where {N}
     @inbounds for k in 1:N, j in 1:N, i in 1:N
         m.hessian[i,j,k] = p[2]     * ( (mod1(i+1, N)==j) -  (mod1(i-2, N)==j)) *  (mod1(i-1, N)==k) + p[2]     * ( (mod1(i+1, N)==k) -  (mod1(i-2, N)==k)) *  (mod1(i-1, N)==j)
     end
