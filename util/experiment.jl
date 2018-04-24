0da6fe6f336f65529467b03bf66c3ebb8275d262
Sace Andro <nskia_bens_cbw@yahoo.co.jp>
2018-04-25 07:52:15 +0900

    added errorbar

--- util/experiment.jl
+++ util/experiment.jl
@@ -19,7 +19,7 @@ assimilate_experiment!(model::Model{N,L}, observed_file::String, obs_variance, d
     nothing
 end
 
-function plot_twin_experiment_result(a::Adjoint{N}, tob) where {N}
+function plot_twin_experiment_result(a::Adjoint{N}, tob, stddev) where {N}
     white_panel = Theme(panel_fill="white")
     p_stack = Array{Gadfly.Plot}(0)
     t = collect(0.:a.dt:a.steps*a.dt)
@@ -27,9 +27,21 @@ function plot_twin_experiment_result(a::Adjoint{N}, tob) where {N}
         df_tob = DataFrame(t=t, x=tob[_i,:], data_type="true orbit")
         _mask = isfinite.(a.obs[_i,:])
         df_obs = DataFrame(t=t[_mask], x=a.obs[_i,:][_mask], data_type="observed")
-        df_assim = DataFrame(t=t, x=a.x[_i,:], data_type="assimilated")
-        df_all = vcat(df_tob, df_obs, df_assim)
-        p_stack = vcat(p_stack, Gadfly.plot(df_all, x="t", y="x", color="data_type", Geom.line, Scale.color_discrete_manual("blue", "red", "green"), Guide.colorkey(title=""), Guide.xlabel("<i>t</i>"), Guide.ylabel("<i>x<sub>$_i</sub></i>"), white_panel))
+        xmin = similar(a.x[_i,:], a.steps+1)
+        xmin .= NaN
+        xmax = similar(a.x[_i,:], a.steps+1)
+        xmax .= NaN
+        xmin[1] = a.x[_i,1]-stddev[_i]
+        xmax[1] = a.x[_i,1]+stddev[_i]
+        df_assim = DataFrame(t=t, x=a.x[_i,:], xmin=xmin, xmax=xmax, data_type="assimilated")
+        p_stack = vcat(p_stack,
+        Gadfly.plot(
+        layer(df_tob, x="t", y="x", color=:data_type, Geom.line),
+        layer(df_obs, x="t", y="x", color=:data_type, Geom.point),
+        layer(df_assim, x="t", y="x", ymin="xmin", ymax="xmax", color=:data_type, Geom.line, Geom.errorbar),
+        Guide.xlabel("<i>t</i>"),
+        Guide.ylabel("<i>x<sub>$_i</sub></i>"),
+        white_panel))
     end
     draw(PDF("lorenz.pdf", 24cm, 40cm), vstack(p_stack))
     nothing
@@ -39,7 +51,7 @@ function twin_experiment!(model::Model{N,L,T}, obs::AbstractMatrix{T}, obs_varia
     a = Adjoint(dt, obs_variance, obs, L-N)
     assimres, minres = assimilate!(a, model, dists, trials)
     print_twin_experiment_result(assimres, minres.minimum, true_params, tob)
-    plot_twin_experiment_result(a, tob)
+    plot_twin_experiment_result(a, tob, get(assimres.stddev))
 end
 
 function twin_experiment!(model::Model{N,L,T}, observed_file::String, obs_variance::T, dt::T, true_params::AbstractVector{T}, true_file::String, dists, trials=10) where {N,L,T<:AbstractFloat}
