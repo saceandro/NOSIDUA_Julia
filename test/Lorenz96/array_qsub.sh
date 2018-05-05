fb611d4564c1afba15da521c840795e911a94980
Sace Andro <nskia_bens_cbw@yahoo.co.jp>
2018-05-05 22:01:02 +0900

    added array job pipeline

---
+++ test/Lorenz96/array_qsub.sh
@@ -0,0 +1,16 @@
+#$ -cwd
+#$ -e /home/konta/.ugeerr
+#$ -o /home/konta/.ugeout
+#$ -S /usr/local/bin/zsh
+##$ -m es -M konta@hgc.jp
+#$ -V
+#$ -l s_vmem=1G,mem_req=1G
+#$ -r no
+
+taskid_1=$(($SGE_TASK_ID - 1))
+replicate=$(($taskid_1 / 50 + 1))
+iter=$(($taskid_1 % 50 + 1))
+outfile=result/replicate_${replicate}/iter_${iter}/RMSE
+errfile=log/replicate/replicate_${replicate}/iter_${iter}
+
+./builddir/replicate_ccompile_each $replicate $iter 1> $outfile 2> $errfile
