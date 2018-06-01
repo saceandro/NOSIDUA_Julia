ec31a2b6d4ad947bb45fc2b6def04c7c9de88b8a
Sace Andro <nskia_bens_cbw@yahoo.co.jp>
2018-06-01 13:07:39 +0900

    modified params and given data to match michaelis case

--- test/michaelis/michaelis.jl
+++ test/michaelis/michaelis.jl
@@ -8,39 +8,51 @@ export julia_main
 
 include("../../util/optparser.jl")
 include("../../util/experiment_ccompile.jl")
+include("model.jl")
 
-function dxdt!(m::Model{N}, t, x, p) where {N}
-    @inbounds m.dxdt[1]     = p[2] * (x[2]   - x[N-1]) * x[N]   + p[1] - x[1]
-    @inbounds m.dxdt[2]     = p[2] * (x[3]   - x[N])   * x[1]   + p[1] - x[2]
-    @inbounds for i in 3:N-1
-        m.dxdt[i] = p[2] * (x[i+1] - x[i-2]) * x[i-1] + p[1] - x[i]
-    end
-    @inbounds m.dxdt[N]     = p[2] * (x[1]   - x[N-2]) * x[N-1] + p[1] - x[N]
-    nothing
-end
+function twin_experiment_obs_m_logging!( # twin experiment with true and obs data logging
+    dxdt!::Function,
+    jacobian!::Function,
+    hessian!::Function;
+    dir = nothing,
+    true_params = nothing,
+    initial_lower_bounds = nothing,
+    initial_upper_bounds = nothing,
+    obs_variance = nothing,
+    obs_iteration = nothing,
+    dt = nothing,
+    spinup = nothing,
+    duration = nothing,
+    generation_seed = nothing,
+    trials = nothing,
+    replicates = nothing,
+    iter = nothing
+    )
 
-function jacobian!(m::Model{N}, t, x, p) where {N} # might be faster if SparseMatrixCSC is used. L=N+M. need to write in one line
-    @inbounds for j in 1:N, i in 1:N
-        m.jacobian[i,j] = p[2]     * ( (mod1(i+1, N) == j) -  (mod1(i-2, N) == j)) * x[mod1(i-1, N)] + p[2]     * (x[mod1(i+1, N)]      - x[mod1(i-2, N)])      *  (mod1(i-1, N) == j) - (i   == j)
+    L = length(initial_lower_bounds)
+    N = L - length(true_params)
+    model = Model(typeof(obs_variance), N, L, dxdt!, jacobian!, hessian!)
+    srand(generation_seed)
+    dists = [Uniform(initial_lower_bounds[i], initial_upper_bounds[i]) for i in 1:L]
+    x0 = rand.(view(dists, 1:N))
+    a = Adjoint(dt, duration, obs_variance, x0, copy(true_params), replicates)
+    orbit!(a, model)
+    dir *= "/true_params_$(join(true_params, "_"))/initial_lower_bounds_$(join(initial_lower_bounds, "_"))/initial_upper_bounds_$(join(initial_upper_bounds, "_"))/spinup_$spinup/generation_seed_$generation_seed/trials_$trials/obs_variance_$obs_variance/obs_iteration_$obs_iteration/dt_$dt/duration_$duration/replicates_$replicates/iter_$iter/"
+    mkpath(dir)
+    writedlm(dir * "true.tsv", a.x')
+    srand(hash([true_params, initial_lower_bounds, initial_upper_bounds, obs_variance, obs_iteration, dt, spinup, duration, generation_seed, trials, replicates, iter]))
+    d = Normal(0., sqrt(obs_variance))
+    a.obs[1,:,:] .= NaN
+    for _replicate in 1:replicates
+        a.obs[2,:,_replicate] .= view(a.x, 2, :) .+ rand(d, a.steps+1)
+        for _i in 1:obs_iteration:a.steps
+            for _k in 1:obs_iteration-1
+                a.obs[2, _i + _k, _replicate] = NaN
+            end
+        end
+        writedlm(dir * "observed$_replicate.tsv", view(a.obs, :, :, _replicate)')
     end
-    @inbounds m.jacobian[:,N+1] .= 1.
-    @inbounds for i in 1:N
-        m.jacobian[i,N+2] = (x[mod1(i+1, N)]      - x[mod1(i-2, N)])      * x[mod1(i-1, N)]
-    end
-    nothing
-end
-
-function hessian!(m::Model{N}, t, x, p) where {N}
-    @inbounds for k in 1:N, j in 1:N, i in 1:N
-        m.hessian[i,j,k] = p[2]     * ( (mod1(i+1, N)==j) -  (mod1(i-2, N)==j)) *  (mod1(i-1, N)==k) + p[2]     * ( (mod1(i+1, N)==k) -  (mod1(i-2, N)==k)) *  (mod1(i-1, N)==j)
-    end
-    @inbounds for j in 1:N, i in 1:N
-        m.hessian[i,j,N+2] = ( (mod1(i+1, N)==j) -  (mod1(i-2, N)==j)) * x[mod1(i-1, N)] + (x[mod1(i+1, N)]    - x[mod1(i-2, N)])    *  (mod1(i-1, N)==j)
-    end
-    @inbounds for k in 1:N, i in 1:N
-        m.hessian[i,N+2,k] = m.hessian[i,k,N+2]
-    end
-    nothing
+    twin_experiment!(dir, a, model, true_params, dists, trials)
 end
 
 Base.@ccallable function julia_main(args::Vector{String})::Cint
--- util/experiment_ccompile.jl
+++ util/experiment_ccompile.jl
@@ -77,6 +77,51 @@ function twin_experiment!(
     twin_experiment!(dir, a, model, true_params, dists, trials)
 end
 
+function twin_experiment_logging!( # twin experiment with true and obs data logging
+    dxdt!::Function,
+    jacobian!::Function,
+    hessian!::Function;
+    dir = nothing,
+    true_params = nothing,
+    initial_lower_bounds = nothing,
+    initial_upper_bounds = nothing,
+    obs_variance = nothing,
+    obs_iteration = nothing,
+    dt = nothing,
+    spinup = nothing,
+    duration = nothing,
+    generation_seed = nothing,
+    trials = nothing,
+    replicates = nothing,
+    iter = nothing
+    )
+
+    L = length(initial_lower_bounds)
+    N = L - length(true_params)
+    model = Model(typeof(obs_variance), N, L, dxdt!, jacobian!, hessian!)
+    srand(generation_seed)
+    dists = [Uniform(initial_lower_bounds[i], initial_upper_bounds[i]) for i in 1:L]
+    x0 = rand.(view(dists, 1:N))
+    a = Adjoint(dt, duration, obs_variance, x0, copy(true_params), replicates)
+    orbit!(a, model)
+    dir *= "/true_params_$(join(true_params, "_"))/initial_lower_bounds_$(join(initial_lower_bounds, "_"))/initial_upper_bounds_$(join(initial_upper_bounds, "_"))/spinup_$spinup/generation_seed_$generation_seed/trials_$trials/obs_variance_$obs_variance/obs_iteration_$obs_iteration/dt_$dt/duration_$duration/replicates_$replicates/iter_$iter/"
+    mkpath(dir)
+    writedlm(dir * "true.tsv", a.x')
+    srand(hash([true_params, initial_lower_bounds, initial_upper_bounds, obs_variance, obs_iteration, dt, spinup, duration, generation_seed, trials, replicates, iter]))
+    d = Normal(0., sqrt(obs_variance))
+    for _replicate in 1:replicates
+        a.obs[:,:,_replicate] .= a.x .+ rand(d, N, a.steps+1)
+        for _i in 1:obs_iteration:a.steps
+            for _k in 1:obs_iteration-1
+                a.obs[:, _i + _k, _replicate] .= NaN
+            end
+        end
+        writedlm(dir * "observed$_replicate.tsv", view(a.obs, :, :, _replicate)')
+    end
+    twin_experiment!(dir, a, model, true_params, dists, trials)
+end
+
+
 function twin_experiment!(outdir::String, a::Adjoint{N,L,K,T}, model::Model{N,L,T}, true_params::AbstractVector{T}, dists, trials=10) where {N,L,K,T<:AbstractFloat}
     tob = deepcopy(a.x) # fixed bug. copy() of >=2 dimensional array is implemented as reference. Thus, deepcopy() is needed.
     println(STDERR, "====================================================================================================================")
